5a81fcca187c9c8df4452d1cc8debe9c03324be5
Vykstorm <victorruizgomezdev@gmail.com>
2017-01-03 23:34:28 +0100

    Añadido script con métodos auxiliares

---
+++ utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: iso8859-1 -*-
+# Autor: Víctor Ruiz Gómez
+# Descripción: Este script define métodos y variables útiles para usarlos en los algoritmos MinMax
+
+
+# Es una variable de tipo float que representa el infinito.
+Inf = float('inf');
+
+
